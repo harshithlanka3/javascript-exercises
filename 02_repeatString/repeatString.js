f240cc42e9944935498a1015002e49828f3a1d21
Harshith Lanka <98058276+harshithlanka3@users.noreply.github.com>
2022-12-20 10:50:49 +0530

    Solved problem HelloWorld and RepeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) return 'ERROR';
 
+    let final = '';
+    for (let i = 0; i < num; i++) {
+        final += str;
+    }
+    return final;
 };
 
 // Do not edit below this line
