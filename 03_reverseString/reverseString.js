02dfaaa1610edc7322a68e2ad623dea31a057b89
Harshith Lanka <98058276+harshithlanka3@users.noreply.github.com>
2022-12-20 10:57:18 +0530

    Solved reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let final = '';
 
+    for (let i = input.length - 1; i >= 0; i--) {
+        final += input.charAt(i);
+        input = input.slice(0, i);
+    }
+
+    return final;
 };
 
 // Do not edit below this line
